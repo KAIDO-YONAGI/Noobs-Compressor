8bd67e966fc6a05bb751ae41dd93ae53930a2210
red-shinku <3480198982@qq.com>
2025-10-23 17:02:49 +0800

    修改获取缓冲块函数的返回值，为数据块列表指针

--- Schedule/include/Datacmnctor.h
+++ Schedule/include/Datacmnctor.h
@@ -4,14 +4,14 @@
 #include "../../namespace/namespace_sfc.h"
 
 /**
- * ���ݴ���ӿ�
- * �������ֱ��𷵻�������б���������б���
- * ���麯�������ݿ���Ӧ�ü̳���������ಢʵ��
- * ��Ӧ������
- * ���ڣ����꣬
- * �ýӿڿ�����Ϊһ��ָ�봫��Worker�ӿڵ�work������
- * Ҳ�����ڲ��Խӿڵ�ʵ���ھ�ȡ�����ݿ飬�����Ǵ��ݸ�
- * work������
+ * 数据传输接口
+ * 有两个分别负责返回输入块列表与输出块列表的
+ * 纯虚函数。数据块类应该继承这个抽象类并实现
+ * 相应方法。
+ * 现在，伊玛，
+ * 该接口可能作为一个指针传给Worker接口的work函数，
+ * 也可能在策略接口的实现内就取出数据块，将它们传递给
+ * work函数。
  */
 
 class Datacmnctor 
@@ -22,8 +22,8 @@ public:
 private:
 
 public:
-    virtual sfc::blocks_t get_input_blocks() = 0;
-    virtual sfc::blocks_t get_output_blocks() = 0;
+    virtual sfc::blocks_t* get_input_blocks() = 0;
+    virtual sfc::blocks_t* get_output_blocks() = 0;
 };
 
 #endif //DATACOMUNICATOR_H_INTERFACE
