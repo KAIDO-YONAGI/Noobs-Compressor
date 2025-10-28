1394bda8840fa5b83960b7fc7e70890d7a63f659
red-shinku <3480198982@qq.com>
2025-10-28 11:58:14 +0800

    添加线程任务队列

--- namespace/namespace_sfc.h
+++ namespace/namespace_sfc.h
@@ -2,6 +2,8 @@
 #define NAMESPACE_SFC_H
 
 #include <vector>
+#include <queue>
+#include <functional>
 
 /**
  * 命名空间
@@ -16,6 +18,8 @@ namespace sfc{
     using byte = unsigned char;
     using block_t = std::vector<unsigned char>;
     using blocks_t = std::vector< block_t >;
+
+    using task_queue = std::queue< std::function<void()> >;
 }
 
 #endif //NAMESPACE_SFC_H
