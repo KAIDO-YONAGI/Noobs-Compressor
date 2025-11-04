d656e1b6534a4e484dffa2a46763b18c7d41a5c0
KAIDO-YONAGI <1224855487@qq.com>
2025-11-05 00:59:21 +0800

    ReaderUpdate

--- DataStream/DataCommunication/include/HeaderReader.h
+++ DataStream/DataCommunication/include/HeaderReader.h
@@ -1,12 +1,12 @@
 #ifndef HEADERREADER
 #define HEADERREADER
 
-#include<fstream>
-#include<filesystem>
-#include<vector>
-#include<iostream>
-#include<cstring>
-#include<string>
+#include <fstream>
+#include <filesystem>
+#include <vector>
+#include <iostream>
+#include <cstring>
+#include <string>
 #include <dirent.h>
 #include <sys/stat.h>
 #include <cstdint>
@@ -14,47 +14,100 @@
 namespace fs = std::filesystem;
 //对POSIX文件方案启用宏定义
 
-#define POSIX_DIR            DIR
-#define POSIX_DIRENT         dirent
-#define POSIX_STAT           struct stat
-#define POSIX_OPENDIR        opendir
-#define POSIX_READDIR        readdir
-#define POSIX_CLOSEDIR       closedir
-#define POSIX_STAT_FUNC      stat
-#define POSIX_S_ISDIR(mode)  S_ISDIR(mode)
+#define POSIX_DIR DIR
+#define POSIX_DIRENT dirent
+#define POSIX_STAT struct stat
+#define POSIX_OPENDIR opendir
+#define POSIX_READDIR readdir
+#define POSIX_CLOSEDIR closedir
+#define POSIX_STAT_FUNC stat
+#define POSIX_S_ISDIR(mode) S_ISDIR(mode)
 
+namespace fs = std::filesystem;
 
-namespace fs = std::filesystem; 
+class Locator
+{
+public:
+    void relativeLocator(std::ofstream &File, int offset);
+    void relativeLocator(std::ifstream &File, int offset);
+    void relativeLocator(std::fstream &File, int offset) = delete; //防止发生具有歧义的fstream重载
+};
+class FilePath
+{
+private:
+    const char *outPutFilePath;
+    const char *filePathToScan;
 
-class Locator{
-    public:
-        void relativeLocator(std::ofstream& File,int offset);
-        void relativeLocator(std::ifstream& File,int offset);
-        void relativeLocator(std::fstream& File, int offset) = delete;//防止发生具有歧义的fstream重载
+public:
+    FilePath(const char *outPutFilePath, const char *filePathToScan)
+    {
+        this->outPutFilePath = outPutFilePath;
+        this->filePathToScan = filePathToScan;
+    }
+    const char *getOutPutFilePath() const { return outPutFilePath; }
+    const char *getFilePathToScan() const { return filePathToScan; }
+};
+class FileDetails
+{
+private:
+    std::string name;
+    std::string fullPath;
+
+    uint8_t sizeOfName;
+    uint64_t fileSize;
+    bool isFile;
+
+public:
+    const std::string &getName() const { return name; }
+    const std::string &getFullPath() const { return fullPath; }
+
+    uint8_t getSizeOfName() const { return sizeOfName; }
+
+    uint64_t getFileSize() const { return fileSize; }
+
+    bool getIsFile() const { return isFile; }
+
+    FileDetails(std::string name, uint8_t sizeOfName, uint64_t fileSize, bool isFile, std::string fullPath)
+    {
+        this->name = name;
+        this->sizeOfName = sizeOfName;
+        this->fileSize = fileSize;
+        this->isFile = isFile;
+        this->fullPath = fullPath;
+    }
+};
+class BinaryIO
+{
+private:
+    FilePath &File;
+
+public:
+    BinaryIO(FilePath &File) : File(File) {}
+    uint64_t getFileSize(const char *filePathToScan);
+    void scanner();
+    void WriteBinaryStandard(std::ofstream &outfile, FileDetails &details);
+    
 };
 
+void scanFlow(FilePath &File);
 void readerForCompression();
 void readerForDecompression();
 
-void appendMagicStatic(const std::string& outputFilePath);
-void outPutAllPaths(std::string &outPutFilePath, std::string &filePathToScan);
-bool fileIsExist(const std::string &outPutFilePath);
-void scanFlow(std::string &outPutFilePath, std::string &filePathToScan);
-uint64_t getFileSize(const std::string& filepath, uint64_t& outSize);
-
+void appendMagicStatic(const char *outputFilePath);
+bool fileIsExist(const char *outPutFilePath);
 
 template <typename T>
-void write_binary_le(std::ofstream& file, T value) {
-    file.write((const char*)&value, sizeof(T));
+void write_binary_le(std::ofstream &file, T value)
+{
+    file.write((const char *)&value, sizeof(T));
 }
 
 template <typename T>
-T read_binary_le(std::ifstream& file) {
+T read_binary_le(std::ifstream &file)
+{
     T value;
-    file.read((char*)&value, sizeof(T));
+    file.read((char *)&value, sizeof(T));
     return value;
 }
 
-
 #endif
-
--- DataStream/DataCommunication/src/HeaderReader.cpp
+++ DataStream/DataCommunication/src/HeaderReader.cpp
@@ -6,110 +6,134 @@
 // void Locator:: relativeLocator(std::ifstream& File,int offset){
 //     File.seekg(File.tellg()+offset,File.beg);
 // }
-void readerForCompression(){
-    std::vector<std::string> tempDirectoryPath;
-}
-void readerForDecompression(){
-    std::vector<std::string> tempDirectoryPath;
-}
+// void readerForCompression(){
+//     std::vector<std::string> tempDirectoryPath;
+// }
+// void readerForDecompression(){
+//     std::vector<std::string> tempDirectoryPath;
+// }
 
-bool fileIsExist(const char* outPutFilePath){
-    FILE* file=fopen(outPutFilePath,"r");
-    if(file){
+bool fileIsExist(const char *outPutFilePath)
+{
+    FILE *file = fopen(outPutFilePath, "r");
+    if (file)
+    {
         fclose(file);
         return true;
     }
-    else return false;
+    else
+        return false;
 }
 
-bool isFile(const char* path){
+bool isFile(const char *outPutFilePath)
+{
     struct stat statbuf;
-    
-    if (stat(path, &statbuf) != 0) {
-        std::cerr <<(("error_stat(): " + std::string(path)).c_str());
+    if (stat(outPutFilePath, &statbuf) != 0)
+    {
+        std::cerr << (("error_stat(): " + std::string(outPutFilePath)).c_str());
         return false;
     }
     return S_ISREG(statbuf.st_mode);
-} 
+}
 
-void appendMagicStatic(const char* outputFilePath) {
+void appendMagicStatic(const char *outputFilePath)
+{
     // 以二进制追加模式打开文件
     std::ofstream outFile(outputFilePath, std::ios::binary | std::ios::app);
-    
-    if (!outFile) {
-        std::cerr << "Can't open output file: " << outputFilePath <<"\n";
+
+    if (!outFile)
+    {
+        std::cerr << "Can't open output file: " << outputFilePath << "\n";
         return;
     }
 
     // 写入32位魔数（0xDEADBEEF）
     const uint32_t magic = 0xDEADBEEF;
-    outFile.write((const char*)&magic, sizeof(magic));
+    outFile.write((const char *)&magic, sizeof(magic));
 
-    if (!outFile) {
-        std::cerr << "Error:Can't write magic num in file" << "\n";
+    if (!outFile)
+    {
+        std::cerr << "Error:Can't write magic num in file"
+                  << "\n";
     }
     outFile.close();
 }
-uint64_t getFileSize(const char* filepath) {
+uint64_t BinaryIO::getFileSize(const char *filePathToScan)
+{
     struct stat stat_buf;
     uint64_t outSize;
-    if (stat(filepath, &stat_buf) != 0) {
-        std::cerr <<("error_stat()"); // 打印错误
+    if (stat(filePathToScan, &stat_buf) != 0)
+    {
+        std::cerr << ("error_stat()"); // 打印错误
     }
     outSize = stat_buf.st_size;
     return outSize;
 }
-void outPutAllPaths(const char* outPutFilePath, const char* filePathToScan)
+void BinaryIO::scanner()
 {
-    POSIX_DIR *dir=opendir(filePathToScan);
+    POSIX_DIR *dir = opendir(File.getFilePathToScan());
     struct POSIX_DIRENT *entry;
-    
-    if (!dir) {
-        std::cerr <<("error_failToOpenFile");
+    if (!dir)
+    {
+        std::cerr << ("error_failToOpenFile");
         return;
     }
-    std::ofstream file(outPutFilePath, std::ios::binary | std::ios::app);
-    while ((entry = readdir(dir)) != NULL) {
+    std::ofstream outfile(File.getOutPutFilePath(), std::ios::binary | std::ios::app);
+    while ((entry = readdir(dir)) != NULL)
+    {
         std::string name = entry->d_name;
-        std::string fullPath = std::string(filePathToScan) + "\\" + name; //绝对路径
-        uint8_t sizeOfName=name.size();
-        bool is_File=isFile(fullPath.c_str());
+        std::string fullPath = std::string(File.getFilePathToScan()) + "\\" + name; //绝对路径
+        uint8_t sizeOfName = name.size();
+        bool is_File = isFile(fullPath.c_str());
+        uint64_t fileSize = getFileSize(fullPath.c_str()); //获取文件大小
 
         if (name == "." || name == "..")
             continue;
 
-        write_binary_le(file,sizeOfName);//文件名偏移量
-        file.write(name.c_str(),sizeOfName);//文件名
-
-        file.write(is_File?"1":"0",1);
-        
-        if(is_File){
-            uint64_t fileSize=getFileSize(fullPath.c_str());
-            write_binary_le(file,fileSize);
-            write_binary_le(file,uint64_t(0));
-        }
-        else if(!is_File){
-        }
+        FileDetails details(name, sizeOfName, fileSize, is_File, fullPath); //保存文件信息
+        WriteBinaryStandard(outfile, details);                              //写入存储协议
     }
-    file.close();
+    outfile.close();
     POSIX_CLOSEDIR(dir);
+    return;
 }
+void BinaryIO::WriteBinaryStandard(std::ofstream &outfile, FileDetails &details)
+{
+    if (details.getIsFile())
+    {
+        uint8_t SizeOfName = details.getSizeOfName();
 
-void scanFlow(const char* outPutFilePath, const char* filePathToScan){
-    if(fileIsExist(outPutFilePath)){
-        std::cerr <<("Error:fileIsExist \nTry to clear:" + std::string(outPutFilePath));
+        write_binary_le(outfile, SizeOfName);                 //文件名偏移量
+        outfile.write(details.getName().c_str(), SizeOfName); //文件名
+        outfile.write("1", 1);                                //文件标记
+        write_binary_le(outfile, details.getFileSize());      //压缩前大小
+        write_binary_le(outfile, uint64_t(0));                //预留64位，压缩完成后使用
     }
-    appendMagicStatic(outPutFilePath);
-    outPutAllPaths(outPutFilePath, filePathToScan);
-    appendMagicStatic(outPutFilePath);
+    else if (!details.getIsFile())
+    {
+        outfile.write("0", 1);
+    }
+}
+void scanFlow(FilePath &File)
+{
+    if (fileIsExist(File.getOutPutFilePath()))
+    {
+        std::cerr << ("Error:fileIsExist \nTry to clear:" + std::string(File.getOutPutFilePath()));
+    }
+    BinaryIO headerReader(File);
+    appendMagicStatic(File.getOutPutFilePath());
+    headerReader.scanner();
+    appendMagicStatic(File.getOutPutFilePath());
 }
 
 int main()
 {
-    const char* outPutFilePath="FilesList.bin";
-    const char* filePathToScan="D:\\1gal";
+    const char *outPutFilePath = "FilesList.bin";
+    const char *filePathToScan = "D:\\1gal";
     
-    scanFlow(outPutFilePath, filePathToScan);
+    FilePath File(outPutFilePath, filePathToScan);
+
+    scanFlow(File);
 
     system("pause");
     return 0;
