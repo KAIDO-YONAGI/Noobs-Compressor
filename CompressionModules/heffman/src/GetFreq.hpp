bcf91d7fd3a8bb80e8618441b20421579405b2df
red-shinku <3480198982@qq.com>
2025-11-19 20:27:41 +0800

    频率统计模块基本完成。封装线程池，编写多线程处理

--- CompressionModules/heffman/src/GetFreq.hpp
+++ CompressionModules/heffman/src/GetFreq.hpp
@@ -1,8 +1,15 @@
 #ifndef GETFREQ_H
 #define GETFREQ_H
 
-#include "../include/Heffman.h"
 #include <thread>
+#include <future>
+#include <memory>
+#include <string>
+#include "../include/Heffman.h"
+#include "../../../ThreadPool/ThreadPool.h"
+
+//临时
+#define MAX_NUMS_OF_THREAD 8
 
 /**
  * 赫夫曼统计字符频率的功能模块
@@ -15,58 +22,80 @@
  * 
  * 成员函数：
  *    私有：
- *    work()：
- *        交给线程执行的任务函数，是实际操作
+ *    work(int)：
+ *        实际操作，调用统计字符功能
+ *    gen_task(int):
+ *        生成packaged_task包装的任务函数，传给线程池
  * 
  *    用户接口：
  *    work(Datacmnctor*)：
- *        将数据块指针保存在类内，
+ *        将数据块指针保存在类内
  *    
  */
 
 class GetFreq: public Worker 
 {
 public:
-    GetFreq(Heffman*, int*);
+    GetFreq(Heffman*);
     ~GetFreq();
 
 private:
-    Heffman *heffman;
-    int *core_use_count;
-
-    void work();
+    std::shared_ptr<Heffman> heffman; 
+    std::unique_ptr<ThreadPool> tpool;
+    sfc::blocks_t* in_block;
+    sfc::blocks_t* out_block;
+    
+    void work(const int&);
+    std::packaged_task<void()> gen_task(const int&);
     
 public:
     void work(Datacmnctor*) override;
 };
 
-inline GetFreq::GetFreq(Heffman* heffcore, int *count):
-    heffman(heffcore), core_use_count(count) 
-    {  
-        ++*core_use_count;
-    }
+inline GetFreq::GetFreq(Heffman* heffcore):
+    heffman(heffcore), tpool(new ThreadPool(MAX_NUMS_OF_THREAD))
+{ }
 
 inline GetFreq::~GetFreq()
-{
-    --*core_use_count;
-    if(*core_use_count == 0){
-        delete core_use_count;
-        delete heffman;
-    }
-}
+{ }
 
 void GetFreq::work(Datacmnctor *datacmnctor)
 {
-    sfc::blocks_t* inputblocks = datacmnctor->get_input_blocks();
-    
-    if(inputblocks->size() == 1)
+    in_block = datacmnctor->get_input_blocks();
+    out_block = datacmnctor->get_output_blocks();
+    if(in_block->size() == 1)
     {
-        heffman->statistic_freq(1, inputblocks);
-    } 
+        heffman->statistic_freq(1, in_block);
+    }
     else 
     {
         //多线程（需要阻塞主线程）
+        std::vector<std::future<void>> results;
+        for(int i = 1; i <= in_block->size(); ++i)
+        {
+            auto task = gen_task(i);
+            results.push_back(task.get_future());
+            tpool->new_thread(std::to_string(i));
+            tpool->add_task(std::to_string(i), task);
+        }
+        for(auto& result : results)
+        {
+            result.get();
+        }
     }
+}
+
+void GetFreq::work(const int& i)
+{
+    heffman->statistic_freq(i, in_block); 
+}
+
+std::packaged_task<void()> GetFreq::gen_task(const int& i)
+{
+    std::packaged_task<void()> task([this, i]{
+        this->work(i);
+    });
+    return task;
 }
 
 #endif //GETFREQ_H
