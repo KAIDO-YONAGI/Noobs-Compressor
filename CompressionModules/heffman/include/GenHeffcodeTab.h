033360b8cc701281fe1d60929631f401558f5373
red-shinku <3480198982@qq.com>
2025-11-24 17:17:52 +0800

    生成编码表并向外传递的功能模块

--- CompressionModules/heffman/include/GenHeffcodeTab.h
+++ CompressionModules/heffman/include/GenHeffcodeTab.h
@@ -2,25 +2,30 @@
 #define GENHEFFCODEDTAB_H
 
 #include <memory>
+#include <typeinfo>
 #include "Heffman.h"
+#include "../../../DataBlocks/include/HefftabBlock.h"
+#include "../../hefftype/Heffman_type.h"
+#include "../../../Schedule/include/Datacmnctor.h"
 #include "../../Schedule/include/Worker.h"
 #include "../../../ThreadPool/ThreadPool.h"
 
-//TODO: 需要合并多线程结果，生成编码表，将其写到输出块
-
+/**
+ * 赫夫曼功能模块之一：生成编码表。
+ * 先合并多线程频率统计结果，生成编码树，最后
+ * 完善编码表，发送给HefftabBlock
+ */
 class GenHeffcodeTab: public Worker
 {
 public:
-    GenHeffcodeTab(/* args */);
+    GenHeffcodeTab(Heffman*);
     ~GenHeffcodeTab();
 
     void work(Datacmnctor*) override;
 
 private:
     std::shared_ptr<Heffman> heffman;
-    sfc::block_t out_block;
 
-    void outputTab(sfc::block_t&);
 };
 
 
--- CompressionModules/heffman/src/GenHeffcodeTab.cpp
+++ CompressionModules/heffman/src/GenHeffcodeTab.cpp
@@ -1,16 +1,29 @@
 #include "../include/GenHeffcodeTab.h"
 
-GenHeffcodeTab::GenHeffcodeTab(/* args */)
-{
-}
+GenHeffcodeTab::GenHeffcodeTab(Heffman* heffcore):
+heffman(heffcore)
+{ }
 
 GenHeffcodeTab::~GenHeffcodeTab()
-{
-}
+{ }
 
 void GenHeffcodeTab::work(Datacmnctor* datacmnctor)
 {
-    auto tab = datacmnctor->get_value();
-    
+    datacmnctor->ready_put_value();
+    auto type = datacmnctor->get_value();
+    try
+    {
+        HeffmapType& heffmap = dynamic_cast<HeffmapType&>(type);
+        
+        heffman->merge_ttabs();
+        heffman->gen_hefftree();
+        heffman->save_code_inTab();
+        
+        heffmap.hefftab = heffman->getHashtab();
+    }
+    catch(std::bad_cast& e)
+    {
+        //FIXME: 处理向下转型异常
+    }  
 }
 
