07042b9796e316b4f908d3c6b80accf6712356da
YONAGI <1224855487@qq.com>
2026-03-15 01:50:50 +0800

    rename

--- CompressorFileSystem/DataCommunication/include/BinaryStandardLoader.h
+++ CompressorFileSystem/DataCommunication/include/BinaryStandardLoader.h
@@ -47,9 +47,9 @@ private:
     Y_flib::DataBlock buffer =
         Y_flib::DataBlock(BUFFER_SIZE + 1024); // 私有buffer,预留1024字节防止溢出
 
-    void requestDone();                                                                                                 // 标记块读取完成
-    void allLoopDone();                                                                                                 // 标记所有循环完成并清理资源
-    void loadBySeparatedStandard(StandardsReader &standardsReader, Y_flib::FileCount &countOfChildDirectory, Aes &aes); // 读取单个数据块、解密、解析
+    void setRequestDone();                                                                                                 // 标记块读取完成
+    void setAllLoopDone();                                                                                                 // 标记所有循环完成并清理资源
+    void loadEntryBlock(StandardsReader &standardsReader, Y_flib::FileCount &countOfChildDirectory, Aes &aes); // 读取单个数据块、解密、解析
     void loadHeaderStandard(std::ifstream &inFile, Header &header, Y_flib::DataBlock &buffer);
     void loadSeparatedStandard(FlagType &flag, StandardsReader &standardsReader, Y_flib::IvSize &ivNum);
 
@@ -83,7 +83,7 @@ public:
         this->parentPath = parentPath;
     }
 
-    ~BinaryStandardLoader() { allLoopDone(); }
+    ~BinaryStandardLoader() { setAllLoopDone(); }
 
     Y_flib::DirectoryOffsetSize getDirectoryOffset() { return header.directoryOffset; } // 获取目录块偏移量
 
